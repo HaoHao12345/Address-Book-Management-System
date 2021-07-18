e4d778c20540257cdab8d89cdcd213ea1678873b
XXH <50578645+HaoHao12345@users.noreply.github.com>
2021-07-18 15:12:49 +0800

    Create addContactPerson.h

---
+++ addContactPerson.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+#include<cstring>
+#include"structureDesign.h"
+
+using namespace std;
+
+# define MAX 1000 // 通讯录中最多存放的人
+
+
+// 添加联系人功能
+void addContactPerson(AddressBook *ab);
