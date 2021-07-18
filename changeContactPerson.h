5ebbfe4a845949515f5be7e433a976a79f918114
XXH <50578645+HaoHao12345@users.noreply.github.com>
2021-07-18 15:13:25 +0800

    Create changeContactPerson.h

---
+++ changeContactPerson.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+#include<cstring>
+#include"existContactPerson.h"
+#include"structureDesign.h"
+
+using namespace std;
+
+// 修改联系人功能
+void changeContactPerson(AddressBook *ab);
