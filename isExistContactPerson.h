600afb023cb29ff1d88ff617a8e45946b249c982
XXH <50578645+HaoHao12345@users.noreply.github.com>
2021-07-18 15:16:39 +0800

    Create isExistContactPerson.h

---
+++ isExistContactPerson.h
@@ -0,0 +1,9 @@
+#pragma once
+#include"structureDesign.h"
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+// 判断通讯录是否存在某特定联系人功能
+int isExistContactPerson(AddressBook ab,string name);
