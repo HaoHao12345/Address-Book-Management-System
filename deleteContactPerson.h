348e8be9b3875e1eaecd3a55dbb76f34af898a81
XXH <50578645+HaoHao12345@users.noreply.github.com>
2021-07-18 15:14:15 +0800

    Create deleteContactPerson.h

---
+++ deleteContactPerson.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+#include<cstring>
+#include"existContactPerson.h"
+#include"structureDesign.h"
+
+using namespace std;
+
+// 删除联系人功能
+void deleteContactPerson(AddressBook *ab);
